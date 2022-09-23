8bc56c6973a8271d4546abbbeb2f258b146dd23d
shamil <muhammedshamil299@gmail.com>
2022-09-23 11:16:55 +0530

    email verification added

--- Allauth/all_auth/all_auth/settings.py
+++ Allauth/all_auth/all_auth/settings.py
@@ -47,7 +47,17 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 
-ACCOUNT_SIGNUP_REDIRECT_URL = "/success/"
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
+ACCOUNT_EMAIL_REQUIRED = True
+
+ACCOUNT_EMAIL_UNIQUE = True
+
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -142,3 +152,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Email conf
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'wondersofearth91@gmail.com'
+EMAIL_HOST_PASSWORD = 'emxmtkyfjzschsim'
+
+DEFAULT_FROM_EMAIL = 'noreply<wondersofearth91@gmail.com>'
+
