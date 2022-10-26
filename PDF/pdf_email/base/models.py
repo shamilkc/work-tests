354403087511e0d58c4f84c371b69685d358932c
shamil <muhammedshamil299@gmail.com>
2022-10-26 10:19:58 +0530

    file email send

---
+++ PDF/pdf_email/base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Files(models.Model):
+    file = models.FileField()
+
+   
---
+++ PDF/pdf_email/base/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail, EmailMultiAlternatives, EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+from . models import Files
+
+# Create your views here.
+
+def home(request):
+    
+    if request.method =="POST":
+
+        html_content = render_to_string('email.html', ).strip()
+
+        subject = 'PDF ATTACHMENT'
+        recipients = [request.POST['email']]
+        sender = settings.EMAIL_HOST_USER
+        file = request.FILES['file']
+
+        
+
+        msg = EmailMessage(subject, html_content, from_email=sender, to=recipients)
+        msg.content_subtype = 'html'  
+        # msg.mixed_subtype = 'related'  
+        
+        msg.attach(file.name,file.read(),file.content_type)
+        msg.send()
+
+        file_save = Files(file=file)
+        file_save.save()
+        
+        return redirect('home')
+    return render(request,'home.html')
