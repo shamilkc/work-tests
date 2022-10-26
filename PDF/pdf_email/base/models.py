1675bf24ef11c7a303792c48d625f691d1463315
shamil <muhammedshamil299@gmail.com>
2022-10-26 15:40:20 +0530

    ls

--- PDF/pdf_email/base/models.py
+++ PDF/pdf_email/base/models.py
@@ -1,8 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
 
 class Files(models.Model):
+    name = models.CharField(max_length = 200)
     file = models.FileField()
 
    
--- PDF/pdf_email/base/views.py
+++ PDF/pdf_email/base/views.py
@@ -31,3 +31,27 @@ def home(request):
         
         return redirect('home')
     return render(request,'home.html')
+
+
+
+
+def email(request):
+    file = Files.objects.get(id=1)
+    path = file.file.path
+    if request.method =="POST":
+
+        html_content = render_to_string('email.html', ).strip()
+
+        subject = request.POST['subject']
+        recipients = [request.POST['email']]
+        sender = settings.EMAIL_HOST_USER
+
+        msg = EmailMessage(subject, html_content, from_email=sender, to=recipients)
+        msg.content_subtype = 'html'  
+        # msg.mixed_subtype = 'related' 
+
+        msg.attach_file(path)
+        msg.send()
+
+        return redirect('email')
+    return render(request,'emailview.html')
